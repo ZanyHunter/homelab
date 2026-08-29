481aa881a2a057f96545ab82a311e2debe55f598
ZanyHunter <ZanyHunter@users.noreply.github.com>
2026-08-29 03:42:22 +0000

    Deploy preview for PR 38 🛫

--- pr-preview/pr-38/searcher-09f2665d.js
+++ pr-preview/pr-38/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-94df4bd1.js',
+                path_to_root + 'searchindex-0f02ef97.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

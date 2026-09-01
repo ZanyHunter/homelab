462436be89d6d8f47a4db9231da5479b23aa30ab
ZanyHunter <ZanyHunter@users.noreply.github.com>
2026-09-01 13:28:50 +0000

    Deploy preview for PR 69 🛫

--- pr-preview/pr-69/searcher-09f2665d.js
+++ pr-preview/pr-69/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-682606ea.js',
+                path_to_root + 'searchindex-8c4f5006.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

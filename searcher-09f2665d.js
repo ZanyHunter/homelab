5668dc8e221bb9867bdc4e73a130a85762a812d5
ZanyHunter <ZanyHunter@users.noreply.github.com>
2026-09-02 20:05:34 +0000

    deploy: f2e0d081f891ebcb95b8d33b099b9661a7f01782

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6a7a7c88.js',
+                path_to_root + 'searchindex-9c20764e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
